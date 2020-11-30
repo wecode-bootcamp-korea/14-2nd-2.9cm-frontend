a43d602441a505e345e7f81f21991c02e7f8a29a
김태현 <kimtaehyun@gimtaehyeon-ui-MacBookPro.local>
2020-11-30 21:53:39 +0900

    squashed commit

--- src/Pages/Main/Main.js
+++ src/Pages/Main/Main.js
@@ -1,9 +1,567 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import styled, { StyleSheetManager } from "styled-components";
+import Nav from '../../Component/Nav/Nav';
+import Footer from '../../Component/Footer/Footer';
+import { FaArrowRight } from 'react-icons/fa';
+import ProductCard from './ProductCard/ProductCard';
+import SearchModal from './SearchModal/SearchModal';
+import CheckBoxBrand from './CheckBoxBrand/CheckBoxBrand';
+
+// const checkBoxDataList = [
+//   {data: ['나이키', '아디다스', '뉴발란스', '반스', '컨버스']}
+// ]
 
 export default function Main() {
+  const [productData, setProductData] = useState([]);
+  const [isModal, setIsModal] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const [search, setSearch] = useState('');
+  const [check, setCheck] = useState('');
+  const [isSearched, setIsSearched] = useState(false);
+  const [isChecked, setIsChecked] = useState(false);
+
+  useEffect(() => {
+    fetch("/data/productData.json")
+      .then((response) => response.json())
+      .then((response) => {
+        setProductData(response.data);
+        setLoading(false);
+      })
+  }, [])
+
+  const filterPrice = (e) => {
+    console.log(e);
+  }
+
+  const openModal = (e) => {
+    setIsModal(true);
+  }
+
+  const closeModal = (e) => {
+    setIsModal(false);
+  }
+
+  const handleSearch = (e) => {
+    setSearch(e.target.value);
+    if(e.keyCode === 13) {
+      console.log('entered');
+      setIsSearched(true);
+      closeModal();
+    }
+  }
+
+  let filteredProductData = productData.filter((product => {
+    return (
+      product.brand.toLowerCase().includes(search.toLowerCase())
+    )
+  }))
+
+  const handleCheckbox = (e) => {
+    setCheck(e.target.name);
+    setIsChecked(!isChecked);
+  }
+
+  let checkedData = productData.filter((product) => {
+    return (
+      product.brand.toLowerCase().includes(check.toLowerCase())
+    )
+  });
+
   return (
-    <div>
-      
-    </div>
+    <>
+      <Nav 
+        openModal={openModal}
+        closeModal={closeModal}
+        isModal={isModal}
+      />
+      <MainWrapper>
+        <CategoryOption>
+          <CategoryOptionList>
+            <h1>남성신발</h1>
+            <button>NEW</button>
+            <button>BEST</button>
+            <button>스니커즈</button>
+            <button>로퍼</button>
+            <button>구두</button>
+            <button>부츠</button>
+            <button>샌들</button>
+          </CategoryOptionList>
+            <OptionFilter>
+              <Filter>필터</Filter>
+              <Refresh>초기화</Refresh>
+            </OptionFilter>
+          <div>
+            <ProductInfo>
+              <div>상품정보</div>
+              <CheckBoxWrapper>
+                <input
+                type='checkbox'
+                defaultChecked={false}
+                />
+                <PriceLabel
+                  for='cb1'
+                >
+                  무료배송
+                </PriceLabel>
+              </CheckBoxWrapper>
+              <CheckBoxWrapper>
+                <input
+                type='checkbox'
+                defaultChecked={false}
+                />
+                <PriceLabel
+                  for='cb2'
+                >
+                  할인상품만
+                </PriceLabel>
+              </CheckBoxWrapper>
+              <CheckBoxWrapper>
+                <input
+                type='checkbox'
+                defaultChecked={false}
+                />
+                <PriceLabel
+                  for='cb3'
+                >
+                  품절상품 제외
+                </PriceLabel>
+              </CheckBoxWrapper> 
+            </ProductInfo>
+            <PriceList>
+              <div>가격대</div>
+              <PriceWidth>
+                <input
+                type='radio'
+                defaultChecked={false}
+                onClick={filterPrice}
+                />
+                <PriceLabel
+                  for='cb1'
+                >
+                  전체 가격
+                </PriceLabel>
+              </PriceWidth>
+              <PriceWidth>
+                <input
+                type='radio'
+                defaultChecked={false}
+                />
+                <PriceLabel
+                  for='cb2'
+                >
+                  0 ~ 10,000원
+                </PriceLabel>
+              </PriceWidth>
+              <PriceWidth>
+                <input
+                type='radio'
+                defaultChecked={false}
+                />
+                <PriceLabel
+                  for='cb3'
+                >
+                  10,000원 ~ 50,000원 
+                </PriceLabel>
+              </PriceWidth>
+              <PriceWidth>
+                <input
+                type='radio'
+                defaultChecked={false}
+                />
+                <PriceLabel
+                  for='cb4'
+                >
+                  50,0000원 ~ 100,0000원
+                </PriceLabel>
+              </PriceWidth>
+              <PriceWidth>
+                <input
+                type='radio'
+                defaultChecked={false}
+                />
+                <PriceLabel
+                  for='cb5'
+                >
+                  100,0000원 ~ 20,0000원
+                </PriceLabel>
+              </PriceWidth>
+            </PriceList>
+            <BrandListWrapper>
+              <BrandsHeading>브랜드</BrandsHeading>
+              <SearchBox></SearchBox>
+              
+              {/* {checkBoxDataList.map((checkboxData) => {
+                return (
+                  <CheckBoxBrand checkBoxData={checkboxData[0]}/>
+                )
+              })} */}
+              
+              {/* <div>
+                <Brand
+                  type='checkbox'
+                  defaultChecked={false}
+                  name='나이키'
+                  onClick={handleCheckbox}
+                  />
+                <BrandLabel
+                  for='cb1'
+                  name='나이키'
+                >
+                  나이키 (59)
+                </BrandLabel>
+              </div> */}
+
+              <div>
+                <Brand
+                  type='checkbox'
+                  defaultChecked={false}
+                  name='나이키'
+                  onClick={handleCheckbox}
+                  />
+                <BrandLabel
+                  for='cb1'
+                  name='나이키'
+                >
+                  나이키 (59)
+                </BrandLabel>
+              </div>
+              <div>
+                <Brand
+                  type='checkbox'
+                  defaultChecked={false}
+                  name='아디다스'
+                  onClick={handleCheckbox}
+                  />
+                <BrandLabel
+                  for='cb1'
+                  name='아디다스'
+                >
+                  아디다스 (36)
+                </BrandLabel>
+              </div>
+              <div>
+                <Brand
+                  type='checkbox'
+                  defaultChecked={false}
+                  name='뉴발란스'
+                  onClick={handleCheckbox}
+                  />
+                <BrandLabel
+                  for='cb1'
+                  name='뉴발란스'
+                >
+                  뉴발란스 (88)
+                </BrandLabel>
+              </div>
+              <div>
+                <Brand
+                  type='checkbox'
+                  defaultChecked={false}
+                  name='엑셀시오르'
+                  onClick={handleCheckbox}
+                  />
+                <BrandLabel
+                  for='cb1'
+                  name='엑셀시오르'
+                >
+                  엑셀시오르 (21)
+                </BrandLabel>
+              </div>
+              <div>
+                <Brand
+                  type='checkbox'
+                  defaultChecked={false}
+                  name='컨버스'
+                  onClick={handleCheckbox}
+                  />
+                <BrandLabel
+                  for='cb1'
+                  name='컨버스'
+                >
+                  컨버스 (28)
+                </BrandLabel>
+              </div>
+              <div>
+                <Brand
+                  type='checkbox'
+                  defaultChecked={false}
+                  name='캐치볼'
+                  onClick={handleCheckbox}
+                  />
+                <BrandLabel
+                  for='cb1'
+                  name='캐치볼'
+                >
+                  캐치볼 (18)
+                </BrandLabel>
+              </div>
+              <div>
+                <Brand
+                  type='checkbox'
+                  defaultChecked={false}
+                  name='조셉트'
+                  onClick={handleCheckbox}
+                  />
+                <BrandLabel
+                  for='cb1'
+                  name='조셉트'
+                >
+                  조셉트 (8)
+                </BrandLabel>
+              </div>
+            </BrandListWrapper>
+          </div>
+        </CategoryOption>
+        <CategoryList>
+          <ListFilterWrapper>
+            <ListFilter>
+              <FilterAll>
+                전체
+              </FilterAll>
+              <FilterItem>
+                하이탑
+              </FilterItem>
+              <FilterItem>
+                로우탑
+              </FilterItem>
+              <FilterItem>
+                슬립온
+              </FilterItem>
+              <FilterItem>
+                런닝화
+              </FilterItem>
+            </ListFilter>
+            <ListFilterButton>
+              추천순
+            </ListFilterButton>
+          </ListFilterWrapper>
+          {isChecked ? <ProductCard product={checkedData[0]} /> : isSearched ? <ProductCard product={filteredProductData[0]} /> : !!productData.length > 0 && productData.map((product) => {
+            return (
+              <ProductCard key={product.id} id={product.id} product={product}/>
+            )
+          })}
+          
+          <PageChangeWrapper>
+            <PageChange>
+              1 2 3 4 5 6 <FaArrowRight />
+            </PageChange>
+          </PageChangeWrapper>
+        </CategoryList>
+      </MainWrapper>
+      <Footer />
+      {isModal ? 
+        <SearchModal 
+          isModal={isModal}
+          openModal={openModal}
+          closeModal={closeModal}
+          handleSearch={handleSearch}
+        />
+        :
+        ''
+      }
+    </>
   )
 }
+
+const MainWrapper = styled.div`
+  display: flex;
+
+`
+
+const CategoryOption = styled.div`
+  width: 250px;
+  margin-left: 50px;
+
+`
+
+const CategoryOptionList = styled.div`
+  display: flex;
+  flex-direction: column;
+  border-bottom: 4px solid #000;
+  padding-bottom: 40px;
+
+  h1 {
+    font-size: 23px;
+    font-weight: bold;
+    padding: 0 0 20px 0;
+    border-bottom: 4px solid #000;
+    margin: 20px 0;
+  }
+
+  button {
+    font-size: 16px;
+    line-height: 40px;
+    border: none;
+    text-align: start;
+    font-size: 16px;
+    color: #5D5D5D;
+  }
+
+`
+
+const OptionFilter = styled.div`
+  display: flex;
+  justify-content: space-between;
+  padding-top: 20px;
+
+`
+
+const Filter = styled.div`
+  font-size: 18px;
+
+`
+
+const Refresh = styled.div`
+  font-size: 14px;
+  text-decoration: underline;
+  color: #bbb;
+
+`
+
+const ProductInfo = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-top: 40px;
+  font-size: 16px;
+  font-weight: bold;
+  padding-bottom: 20px;
+  border-bottom: 1px solid #ccc;
+
+`
+
+const CheckBoxWrapper = styled.div`
+  margin: 10px 0;
+  font-weight: normal;
+
+`
+
+const PriceLabel = styled.label`
+  margin-left: 5px;
+  font-size: 13px;
+
+`
+
+const PriceList = styled.div`
+  display: flex;
+  flex-direction: column;
+  font-size: 16px;
+  padding: 30px 0 20px 0;
+  font-weight: bold;
+  border-bottom: 1px solid #ccc;
+
+`
+
+const PriceWidth = styled.div`
+  margin: 8px 0;
+  font-size: 12px;
+  font-weight: normal;
+
+`
+
+const BrandListWrapper = styled.div`
+  margin: 30px 0 60px 0;
+  font-size: 16px;
+  font-weight: bold;
+
+`
+
+const BrandsHeading = styled.div`
+  margin: 40px 0 20px 0;
+
+`
+
+const SearchBox = styled.input`
+  width: 100%;
+  height: 40px;
+  border: 1px solid #ccc;
+  margin: 10px 0;
+
+`
+
+const Brand = styled.input`
+  margin: 10px 0;
+
+`
+
+const BrandLabel = styled.label`
+  margin-left: 5px;
+  font-size: 13px;
+  font-weight: normal;
+
+`
+
+const CategoryList = styled.div`
+  width: 100%;
+  margin-right: 100px;
+  box-sizing: border-box;
+
+`
+
+const ListFilterWrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 100%;
+  box-sizing: border-box;
+  height: 50px;
+  margin: 10px 50px 20px 30px;
+  border: 1px solid #ddd;
+  font-size: 14px;
+  color: #bbb;
+
+`
+
+const ListFilter = styled.div`
+  display: flex;
+  align-items: center;
+
+`
+
+const FilterAll = styled.div`
+  margin: 0 20px;
+  padding-right: 20px;
+  border-right: 1px solid #bbb;
+  font-weight: bold;
+  color: #000;
+
+`
+
+const FilterItem = styled(FilterAll)`
+  margin: 0 20px;
+  padding-right: 20px;
+  border-right: 1px solid #bbb;
+  font-weight: normal;
+
+`
+
+const ListFilterButton = styled.button`
+  border: none;
+  text-align: center;
+  height: 100%;
+  padding: 0 30px;
+  border-left: 1px solid #ddd;
+
+`
+
+const PageChangeWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-top: 100px;
+  
+`
+
+const PageChange = styled.div`
+  display:flex;
+  justify-content: space-between;
+  align-items: center;
+  text-align: center;
+  font-size: 48px;
+  margin-right: 20px;
+  width: 300px;
+
+  svg {
+    font-size: 28px;
+  }
+
+`
