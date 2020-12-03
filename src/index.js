e320ed0f323ee7adc8195a841ab8ec5847c33e20
김태현 <kimtaehyun@gimtaehyeon-ui-MacBookPro.local>
2020-12-03 09:13:25 +0900

    squashed commit

--- src/index.js
+++ src/index.js
@@ -1,9 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import GlobalStyles from "./Styles/GlobalStyles";
-
-// import './Styles/common.scss';
-// import './Styles/reset.scss';
 import Routes from './Routes';
 import Theme from "./Styles/theme";
 
